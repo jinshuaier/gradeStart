eb3c115017caf5f5c27562a0ebabb7bd7069b855
jinshuaier <470401910@qq.com>
2017-09-13 10:56:19 +0800

    Initial Commit

---
+++ 评论星星/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  评论星星
+//
+//  Created by 胡高广 on 2017/9/13.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 评论星星/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  评论星星
+//
+//  Created by 胡高广 on 2017/9/13.
+//  Copyright © 2017年 jinshuaier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
